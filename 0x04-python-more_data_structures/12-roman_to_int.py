aa22bf68d999165db4077459e9ef4279c130ec2f
Benjamin <benjminrichie@gmail.com>
2023-12-06 05:18:29 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-def to_subtract(list_num):
-    subtract = 0
-    _list = max(list_num)
-
-    for n in list_num:
-        if _list > n:
-            subtract += n
-
-    return (_list - subtract)
-
-
-def roman_to_int(roman_string):
-    if not roman_string:
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
         return 0
+    storage = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    myNum = [storage[x] for x in roman_string] + [0]
+    x = 0
+
+    for myItems in range(len(myNum) - 1):
+        if myNum[myItems] >= myNum[myItems+1]:
+            x += myNum[myItems]
+        else:
+            x -= myNum[myItems]
+
+    return x
