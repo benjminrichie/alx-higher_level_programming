e602340600e7eaded55c7b4f7dde7a69aacdbc48
Benjamin <benjminrichie@gmail.com>
2023-12-18 05:14:28 -0800

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ import sys
 
 
 def safe_print_integer_err(value):
-    """Prints an integer with "{:d}".format().
+    """""""Prints an integer with "{:d}".format().
 
     If a ValueError message is caught, a corresponding
     message is printed to standard error.
@@ -14,7 +14,7 @@ def safe_print_integer_err(value):
     Returns:
         If a TypeError or ValueError occurs - False.
         Otherwise - True.
-    """
+    """""""
     try:
         print("{:d}".format(value))
         return (True)
