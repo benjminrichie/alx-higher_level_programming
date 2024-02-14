2312012fec993d50b52f24d73e210337f03b1243
Benjamin <benjminrichie@gmail.com>
2024-02-14 07:03:43 -0800

    ALL MANDATORY TASKS

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- This only creates the table force_name on your MySQL server
+-- This only creates a table in a database
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Only creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+-- Only creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Only use a database
+USE hbtn_0d_usa;
+-- Only creates a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
