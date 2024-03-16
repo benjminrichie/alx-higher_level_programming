7e3e8a2ad73e3ef4188d610647b2c7bd83ebf64f
Benjamin <benjminrichie@gmail.com>
2024-03-16 03:22:11 -0700

    11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" This only prints the State object with the name passed as argument from the database
+""" This only prints the State object with the
+name passed as argument from the database
 """
 import sys
 from model_state import Base, State
