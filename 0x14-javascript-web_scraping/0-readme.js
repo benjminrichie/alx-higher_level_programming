145f8c482341b2a48a30720daf28f7afb5fc71c7
Benjamin <benjminrichie@gmail.com>
2024-04-23 07:39:02 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-
-//a script that reads and prints the content of a file.
+/* To read & print contents of a file */
 
 const fs = require('fs');
 const filename = process.argv[2];
-
-fs.readFile(filename, 'utf-8', (error, content) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(content);
-  }
+fs.readFile(filename, 'utf8', (err, data) => {
+  if (err) console.log(err);
+  else console.log(data.toString());
 });
