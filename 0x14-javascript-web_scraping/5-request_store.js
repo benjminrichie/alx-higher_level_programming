5e1eb18f78d96f211734366beaba5c4a93075a5d
Benjamin <benjminrichie@gmail.com>
2024-04-23 07:55:21 -0700

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
-
 /* a script that gets the contents of a webpage and stores it in a file. */
 
 const request = require('request');
 const fs = require('fs');
-const my_url = process.argv[2];
-const my_file = process.argv[3];
+const url = process.argv[2];
+const filename = process.argv[3];
 
-request(my_url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    fs.writeFile(my_file, body, 'utf8', (error) => {
-      if (error) {
-        console.log(error);
-      }
+request(url, (err, response, body) => {
+  if (err) console.log(err);
+  else {
+    fs.writeFile(filename, body, 'utf8', (err) => {
+      if (err) console.log(err);
     });
   }
 });
